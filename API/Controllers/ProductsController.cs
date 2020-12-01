d470e7606e053fca9865af2598ecaeddb26ec214
Vinh Tran <vinhdt90@outlook.com>
2020-12-02 07:36:11 +1100

    initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Data;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return Ok(await _context.Products.FindAsync(id));
+        }
+    }
+}
